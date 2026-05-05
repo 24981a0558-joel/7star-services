29c8a15d4b79376a4ad47e3aa8ce59173295ff82
24981a0558-joel <24981a0558@raghuenggcollege.in>
2026-05-05 10:48:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { store } from "./src/store";
+import AppNavigator from "./src/navigation/AppNavigator";
+import Toast from "react-native-toast-message";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+        <Toast />
+      </NavigationContainer>
+    </Provider>
+  );
+}
